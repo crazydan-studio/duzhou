74c16b99a746dfd25feba6fcd85f8d884aa33ad3
flytreeleft <flytreeleft@crazydan.org>
2024-01-26 11:32:09 +0800

    更新版权声明

--- code/dsl-designer/backend/src/test/java/io/crazydan/duzhou/dsl/designer/test/nop/NopTest.java
+++ code/dsl-designer/backend/src/test/java/io/crazydan/duzhou/dsl/designer/test/nop/NopTest.java
@@ -1,6 +1,6 @@
 /*
- * 渡舟平台 - 致力于构建可演化的全功能型应用生产平台
- * Copyright (C) 2024-2024 [Crazydan Studio](https://studio.crazydan.org)
+ * 渡舟平台 - 致力于构建自运维、自监控、可演化的全功能型应用平台
+ * Copyright (C) 2024 Crazydan Studio <https://studio.crazydan.org>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
